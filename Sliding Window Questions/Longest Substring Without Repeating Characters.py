1565bab5336e0ec8889f9a379f4cca0d1cefcc6b
sri443 <raksdot223@gmail.com>
2026-03-10 21:23:42 +0530

    Create Longest Substring Without Repeating Characters.py

---
+++ Sliding Window Questions/Longest Substring Without Repeating Characters.py
@@ -0,0 +1,22 @@
+#Given a string s, return the length of the longest substring that contains no repeating characters.
+
+#A substring must be contiguous.
+
+def lengthOfLongestSubstring(s):
+  
+    start=0
+    freq=set()
+    max_length=0
+  
+    for end in range(len(s)):
+      
+        while s[end] in freq:
+          
+            freq.remove(s[start])
+            start+=1
+          
+        freq.add(s[end])
+      
+        max_length=max(len(freq),max_length)
+      
+    return max_length
